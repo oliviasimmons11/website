3ef96536ed51d80eaa4eff5ffd4468df29682f23
oliviasimmons11 <134503992+oliviasimmons11@users.noreply.github.com>
2024-06-10 14:49:34 +1200

    layout changes

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+db = SQLAlchemy()
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "this would be the secret key that you make up, don't post this to github obviously"
-class NameForm(FlaskForm):
-   name = StringField("What is your name?", validators=[DataRequired()])
-   submit = SubmitField("Submit Name")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.vtzwgreehjufqdkxmbzr:healthyharbourwatchers@aws-0-ap-southeast-2.pooler.supabase.com:5432/postgres'
+app.config['SECRET_KEY'] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0endncmVlaGp1ZnFka3htYnpyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNTY3Njk5MCwiZXhwIjoyMDMxMjUyOTkwfQ.Dr8taQySTLl1aeSa-3w_gRh3wRvymhGeK-r3zJSFTvw"
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/users'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/users'
-db = SQLAlchemy(app)
+db.init_app(app)
 
 #this creates a model (Sets up how data is to be added into the database)this goes under your configs
 class Users(db.Model):
    id = db.Column(db.Integer, primary_key=True)
-   name = db.Column(db.String(100), nullable=False)
-   email = db.Column(db.String(100), nullable=False, unique=True)
+   name = db.Column(db.String, nullable=False)
+   email = db.Column(db.String, nullable=False, unique=True)
 
 
 #this creates a string to show on the screen what we've just done.
@@ -40,6 +39,10 @@ class UserForm(FlaskForm):
 @app.route('/')
 def index():
    return render_template('home.html', page_title='Home')
+
+@app.route('/carousel')
+def carousel():
+   return render_template('carousel.html', page_title='ABOUT')
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
@@ -68,22 +71,22 @@ def add_user():
 
 
 
-#set up a route for name using methods (plural).
+#set up a route for name using methods (plural)
 #This allows the user to GET information and POST information
 @app.route('/name', methods=['GET', 'POST'])
 def name():
    name = None
    #this means that the first time the user loads the page there won't be a name but once fill out the form it'll put it there
-   form = NameForm() #this is using the form we created above
+   #form = NameForm() #this is using the form we created above
    #validation of the form
-   if form.validate_on_submit():
-       name = form.name.data
-       form.name.data = ''
+   #if form.validate_on_submit():
+       #name = form.name.data
+       #form.name.data = ''
        #if someone submits the form then we assign that to the name variable
        #otherwise it is blank
-   return render_template('name.html',
-           name = name,
-           form = form)
+   #return render_template('name.html',
+           #name = name,
+           #form = form)
 
 
 @app.route('/about')  # note the leading slash, it’s important
